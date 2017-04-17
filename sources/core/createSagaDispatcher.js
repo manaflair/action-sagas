fa242328befce4e737ff2083db15df3c74984b85
Maël Nison <nison.mael@gmail.com>
2017-04-17 10:48:34 +0100

    Saga must be instanciated by the saga middleware

--- sources/core/createSagaDispatcher.js
+++ sources/core/createSagaDispatcher.js
@@ -3,7 +3,7 @@ export function createSagaDispatcher(sagaMiddleware) {
     return store => next => action => {
 
         if (typeof action === `function` && `next` in action.prototype && `throw` in action.prototype)
-            return sagaMiddleware.run(action()).done;
+            return sagaMiddleware.run(action).done;
 
         return next(action);
 
